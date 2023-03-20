ce38e01832cdfb0fe63d8f37de22b48a58fdf437
shafeequ ahmed <shafeequ.ahmed@bitstan.com>
2023-03-20 06:33:43 +0100

    fix ci/cd

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ window.onload = function () {
   const div = document.getElementById("my-div");
   div.onclick = function () {
     const red = Math.floor(Math.random() * 256);
-    const green= Math.floor(Math.random() * 256);
+    const green = Math.floor(Math.random() * 256);
     const blue = Math.floor(Math.random() * 256);
     div.style.backgroundColor = "rgb(" + red + "," + green + "," + blue + ")";
   };

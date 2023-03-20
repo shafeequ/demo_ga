05226adb8ad9ef423815b78e8d6b22d0a2844e0d
shafeequ ahmed <shafeequ.ahmed@bitstan.com>
2023-03-20 06:44:19 +0100

    add ....

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 window.onload = function () {
   const div = document.getElementById("my-div");
   div.onclick = function () {
-    const red= Math.floor(Math.random() * 256);
+    const red = Math.floor(Math.random() * 256);
     const green = Math.floor(Math.random() * 256);
     const blue = Math.floor(Math.random() * 256);
     div.style.backgroundColor = "rgb(" + red + "," + green + "," + blue + ")";
